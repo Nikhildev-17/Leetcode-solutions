246b5823fd7163503b95030679b9694fd50169f2
KOMMINENI VENKATA NIKHIL <komminenivenkatanikhil44@gmail.com>
2026-03-25 11:15:35 -0700

    Time: 0 ms (100%), Space: 9.9 MB (81.76%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void getallSubsets(vector<int>&nums, vector<int>&ans, int i, vector<vector<int>> &allSubsets){
+        if(i == nums.size()){
+            allSubsets.push_back({ans});
+            return;
+        }
+
+        ans.push_back(nums[i]);
+        getallSubsets(nums, ans, i+1, allSubsets);
+
+        ans.pop_back();
+        getallSubsets(nums, ans, i+1, allSubsets);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>>allSubsets;
+        vector<int> ans;
+        getallSubsets(nums, ans, 0, allSubsets);
+
+        return allSubsets;
+    }
+};
