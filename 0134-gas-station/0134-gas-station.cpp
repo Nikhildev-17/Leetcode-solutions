49da69f2dce2b49634609a232163e399ae230497
KOMMINENI VENKATA NIKHIL <komminenivenkatanikhil44@gmail.com>
2026-05-22 19:32:35 +0530

    Time: 5 ms (12.22%), Space: 112.5 MB (23.92%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -3,23 +3,22 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int totalGas = 0;
         int totalCost = 0;
-        int n = gas.size();
-        for(int i=0; i<n; i++){
-            totalCost += cost[i];
-            totalGas += gas[i];
-        }
-
-        if(totalGas < totalCost) return -1;
         int currGas = 0;
         int start = 0;
-        for(int i=0; i<n; i++){
+
+        for(int i=0; i<gas.size(); i++){
+            
+            totalGas += gas[i];
+            totalCost += cost[i];
+
             currGas = currGas + gas[i] - cost[i];
+
             if(currGas < 0){
                 start = i + 1;
                 currGas = 0;
             }
         }
 
-        return start;
+        return totalGas < totalCost ? -1 : start;
     }
 };
