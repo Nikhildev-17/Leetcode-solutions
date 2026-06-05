f566c04a218573122fbeb68758e969de4a6613d2
KOMMINENI VENKATA NIKHIL <komminenivenkatanikhil44@gmail.com>
2026-06-05 10:34:43 +0530

    Time: 26 ms (29.66%), Space: 23.6 MB (33.99%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,10 +14,6 @@ TreeNode* leftCA;
 TreeNode* rightCA;
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
-
-        if(root -> val == p -> val || root -> val == q -> val) return root;
-
-        if((root -> val > p -> val && root -> val < q -> val) || (root -> val < p -> val && root -> val > q -> val)) return root;
          
         if(root -> val > p -> val && root -> val > q -> val){
             return lowestCommonAncestor(root -> left, p, q);
