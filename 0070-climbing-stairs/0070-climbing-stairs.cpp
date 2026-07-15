6abfca4839577a7ae8bdf2880518142d12b0f6a4
KOMMINENI VENKATA NIKHIL <komminenivenkatanikhil44@gmail.com>
2026-07-15 16:38:30 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-
-    int waysToClimbStairs(int n, vector<int>& dp){
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        return dp[n] = waysToClimbStairs(n-1, dp) + waysToClimbStairs(n-2, dp);
-
-    }
-
     int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
         vector<int>dp(n+1, -1);
-        return waysToClimbStairs(n, dp);
+        int i = 3;
+        dp[1] = 1;
+        dp[2] = 2;
+        while(i <= n){
+            dp[i] = dp[i-1] + dp[i-2];
+            i++;
+        }
+
+        return dp[n];
     }
 };
