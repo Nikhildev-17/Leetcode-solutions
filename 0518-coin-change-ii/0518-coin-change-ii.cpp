da74284f122787db337750f0383b64ec0c464f39
KOMMINENI VENKATA NIKHIL <komminenivenkatanikhil44@gmail.com>
2026-08-19 00:25:46 +0530

    Time: 31 ms (49.95%), Space: 36.5 MB (64.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,10 +3,7 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
-        vector<vector<int>> dp(
-            n + 1,
-            vector<int>(amount + 1)
-        );
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
 
         for(int i = 0; i <= n; i++) {
             dp[i][0] = 1;
@@ -17,16 +14,13 @@ public:
 
                 if(coins[i - 1] <= j) {
 
-                    if(dp[i][j - coins[i - 1]]
-                       > INT_MAX - dp[i - 1][j]) {
+                    if(dp[i][j - coins[i - 1]] > INT_MAX - dp[i - 1][j]) {
 
                         dp[i][j] = INT_MAX;
 
                     } else {
 
-                        dp[i][j] =
-                            dp[i][j - coins[i - 1]]
-                            + dp[i - 1][j];
+                        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
                     }
 
                 } else {
